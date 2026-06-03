7abc52facc92fed96eb910ff5ebaab95c987f6b5
sodre77 <douglasodre525@gmail.com>
2026-06-03 19:26:15 -0300

    criação da classe, atributos e metodos etc

--- src/main/java/br/edu/projeto/calculadora/CalculadoraJanela.java
+++ src/main/java/br/edu/projeto/calculadora/CalculadoraJanela.java
@@ -3,6 +3,14 @@ package br.edu.projeto.calculadora;
 import javax.swing.*;
 
 public class CalculadoraJanela extends JFrame {
+    // Componente do ecrã da calculadora
+    private JTextField display;
+    // Instância do teu back-end
+    private Calculadora calculadoraBackend;
 
+    public CalculadoraJanela() {
+        // Inicializa o teu back-end para usarmos mais à frente
+        this.calculadoraBackend = new Calculadora();
 
+    }
 }
